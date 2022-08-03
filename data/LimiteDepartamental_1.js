3109a5805d54c2ba2feb61944af75e74c9f4f80c
fedepanelafondo <37595665+fedepanelafondo@users.noreply.github.com>
2022-08-03 13:56:00 -0500

    Panelas especiales
    1 semana de agosto

--- data/LimiteDepartamental_1.js
+++
@@ -1,8 +0,0 @@
-var json_LimiteDepartamental_1 = {
-"type": "FeatureCollection",
-"name": "LimiteDepartamental_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
